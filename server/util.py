4786f24eb9d50cfbfff93b44c7a77b12590a7fc2
Vimal Mishra <vimal.mishra_cs18@gla.ac.in>
2020-08-05 22:35:25 +0530

    some files added for website

--- server/util.py
+++ server/util.py
@@ -48,5 +48,5 @@ if __name__ == '__main__':
     print(get_location_names())
     print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
     print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
-    print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
-    print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
+    print(get_estimated_price('Kalhalli', 1000, 2, 2))
+    print(get_estimated_price('Ejipura', 1000, 2, 2))
